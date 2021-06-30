641544d378bac876559adefe927f73e5cc922285
Ranjana-Kambhammettu <ranjanak207@gmail.com>
2021-06-30 13:07:23 +0400

    card_view.xml updated

---
+++ app/src/main/java/com/example/myapp/DataClass.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+public class DataClass {
+    String name;
+    String mob;
+    int img_name;
+    DataClass (String mName, String mMob, int mImg_name) {
+        name=mName;
+        mob=mMob;
+        img_name=mImg_name;
+    }
+}
